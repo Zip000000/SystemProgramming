7c3f63f3dbb26d496a6466e6a0e1017de092ad70
Zip <307110017@qq.com>
2019-06-22 21:16:59 +0800

    update

--- 3.Socket/9.save_file_server.c
+++ 3.Socket/9.save_file_server.c
@@ -17,70 +17,71 @@
 #include <pthread.h>
 
 #define MAXBUF 1024
-#define MAXFILENAME 50
+#define MAXFILENAME 200
 
-int is_exit(char* filename) {
+int is_exist(char* filename) {
     if(fopen(filename, "r") == NULL) return 0;
     else return 1;
 }
 
-char* get_newfname(char* oldfname, char* newfname) {
+char* get_newfname(char* fname, char* newfname) {
+    char *tem;
+    char oldfname[MAXFILENAME];
+    if(strrchr(fname, '/') != NULL) {
+        printf("has /\n");
+        tem = strrchr(fname, '/');
+        strcpy(oldfname, tem+1);
+    } else {
+        strcpy(oldfname, fname);
+    }
     char temp[MAXFILENAME];
     strcpy(temp, oldfname);
     strcpy(newfname, temp);
     int count = 1;
     char add[5];
     add[0] = '_';
-    while(is_exit(newfname)) {
+    while(is_exist(newfname)) {
         memset(newfname, 0, sizeof(newfname));
         sprintf(newfname, "%s_%d", oldfname, count);
         count++;
     }
-    //printf("********newname = %s\n", newfname);
     return newfname;
 }
+
 void *saveit(void *arg) {
     int clnt_sock;
     clnt_sock = *(int *)arg;
 
-        printf("进入子线程！\n");
-        char filename[MAXFILENAME];
-        memset(filename, 0, sizeof(filename));
-        int checkfilename;
-        checkfilename = recv(clnt_sock, &filename, MAXFILENAME, 0);
-        send(clnt_sock, &checkfilename, 4, 0);
-        char buf[MAXBUF];
-        FILE *fp_to;
-        char *temp;
-        char oldfname[MAXFILENAME];
-        strcpy(oldfname, filename);
-        if(strrchr(filename, '/') != NULL) {
-            printf("get temp\n");
-            temp = strrchr(filename, '/');
-            printf("temp = %s\n", temp);
-            strcpy(oldfname, temp+1);
+    printf("进入子线程！\n");
+    char filename[MAXFILENAME];
+    memset(filename, 0, sizeof(filename));
+    int checkfilename;
+    checkfilename = recv(clnt_sock, &filename, MAXFILENAME, 0);
+    send(clnt_sock, &checkfilename, 4, 0);
+    char buf[MAXBUF];
+    FILE *fp_to;
+
+    char newfilename[MAXFILENAME];
+    if((fp_to = fopen(get_newfname(filename, newfilename), "wb+")) == NULL) {
+        perror("fopen");
+        exit(1);
     }
-        char newfilename[MAXFILENAME];
-        if((fp_to = fopen(get_newfname(oldfname, newfilename), "wb+")) == NULL) {
-            perror("fopen");
+
+    int ncount;
+    while((ncount = recv(clnt_sock, &buf, MAXBUF, 0)) >=0) {
+        if(ncount == 0) {
+            fclose(fp_to);
+            break;
+        }
+        if((fwrite(buf, 1, ncount, fp_to)) <=0) {
+            perror("fwrite");
             exit(1);
         }
-
-        int ncount;
-        while((ncount = recv(clnt_sock, &buf, MAXBUF, 0)) >=0) {
-            if(ncount == 0) {
-                fclose(fp_to);
-                break;
-            }
-            if((fwrite(buf, 1, ncount, fp_to)) <=0) {
-                perror("fwrite");
-                exit(1);
-            }
-            memset(buf, 0, sizeof(buf));
-        }
-        //printf("PID = %d : ID : %d : newfilename = %s\n", getpid(), my_id, filename);
-        sleep(1);
-        printf("***************************************接受文件成功！文件名为：[%s]\n", newfilename);
+        memset(buf, 0, sizeof(buf));
+    }
+    //printf("PID = %d : ID : %d : newfilename = %s\n", getpid(), my_id, filename);
+    sleep(1);
+    printf("***************************************接受文件成功！文件名为：[%s]\n", newfilename);
     close(clnt_sock);
 
 	return NULL;
