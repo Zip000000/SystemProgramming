7e537cf6a4eea4dd9ac22d9494d1b160ec91fbdb
Zip <307110017@qq.com>
2019-07-08 18:45:53 +0800

    select() practice

--- 5.select_socket/4.select_server_su.c
+++ 5.select_socket/4.select_server_su.c
@@ -15,18 +15,70 @@
 #include <pwd.h>
 #include <sys/ioctl.h>
 
-#define MAX_CLIENT 40
+#define MAX_CLIENT 1000
 #define PORT 8888
 
-typedef struct server_data {
-    int client_cnt;    //一共有多少用户
-    int client_fds[MAX_CLIENT];   //保存每个客户的套接字
-    fd_set all_fds;   //存入所有用户的套接字
-    int maxfd;     //最大的文件描述符
-} server_data;
+typedef struct client_data {
+    int listen_socket;
+    int clientnum;    //当前一共有多少用户
+    int client_socket[MAX_CLIENT];   //保存每个客户的套接字
+    fd_set all_fds;   //存入所有用户的client套接字, 以及listen套接字，select来查找。
+    int maxfdnum;     //目前最大的文件描述符
+} client_data;
 
-server_data my_s_d;
-server_data *s_d = &my_s_d;
+client_data my_c_d;
+client_data *c_data = &my_c_d;
+
+
+int acceptNaddinfo() {
+    
+    //unsigned long ul = 1;
+    //int ioctlcheck = ioctl(c_data->listen_socket, FIONBIO, &ul);
+    
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_size = sizeof(clnt_addr);
+    int clnt_sock;
+    clnt_sock = accept(c_data->listen_socket, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
+    if(clnt_sock < 0) {
+        perror("accept");
+        return -1;
+    }
+    c_data->client_socket[(c_data->clientnum)++] = clnt_sock;
+    return 0;
+}
+
+void rebound() {
+    fd_set *rfds = &(c_data->all_fds);
+    int recvnum = 0;
+    int flag = 0;
+    char str[100];
+    for(int i = 0; i < c_data->clientnum; i++) {
+        int clnt_sock = c_data->client_socket[i];
+        if(clnt_sock == -1) continue;
+        if(FD_ISSET(clnt_sock, rfds)) {
+            printf("clnt可读， 准备读取...\n");
+            recvnum = recv(clnt_sock, str, sizeof(str), 0);
+            if( recvnum > 0 ) {
+                printf("读取成功 读取到的字符串是： %s , 长度： %ld\n", str, strlen(str));
+            }
+            if(recvnum == 0) {
+                printf("有用户登出,暂不做处理\n");
+                close(clnt_sock);
+                continue;
+            }
+            printf("准备发送...\n");
+            if( send(clnt_sock, str, strlen(str), 0) > 0 ){
+                printf("send success\n");
+                printf("反弹成功！\n");
+                flag = 1;
+            } 
+        }
+    }
+    if(flag == 0) printf("反弹失败!\n");
+
+
+}
+
 
 int main() {
     int serv_sock;
@@ -38,32 +90,37 @@ int main() {
     sock_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     sock_addr.sin_port = htons(PORT);
 
-    struct sockaddr_in clnt_addr;
-    socklen_t clnt_addr_size = sizeof(clnt_addr);
-    int clnt_sock;
-    
     int client_fd;
-    fd_set *rfds = &(s_d->all_fds);
     struct timeval tv;
-    
-    int cnt = 0;
+
     bind(serv_sock, (struct sockaddr* )&sock_addr, sizeof(sock_addr));
     listen(serv_sock, 100);
 
+    fd_set *rfds = &(c_data->all_fds);
+    c_data->listen_socket = serv_sock;
+    c_data->clientnum = 0;
+    c_data->maxfdnum = serv_sock;
+    for(int i = 0; i < MAX_CLIENT; i++) {
+        c_data->client_socket[i] = -1;
+    }
+
     while(1) {
-        cnt++;
         tv.tv_sec = 5;
         tv.tv_usec = 0;
         FD_ZERO(rfds);
-        for(int i = 0; i < s_d->client_cnt; i++) {
-            client_fd = s_d->client_fds[i];
-            if(client_fd != -1) {
-                FD_SET(client_fd, rfds);
+        FD_SET(serv_sock, rfds);
+        for(int i = 0; i < c_data->clientnum; i++) {
+            int tmp_socket = c_data->client_socket[i];
+
+            if(tmp_socket != -1) {
+                FD_SET(tmp_socket, rfds);
             }
-            s_d->maxfd = (client_fd > s_d->maxfd ? client_fd : s_d->maxfd);
+            int tmp_max = c_data->maxfdnum;
+            c_data->maxfdnum = (tmp_socket > tmp_max ? tmp_socket : tmp_max);
         }
-        
-        int ret = select(s_d->maxfd+1, rfds, NULL, NULL, &tv);
+
+        int ret = select(c_data->maxfdnum + 1, rfds, NULL, NULL, &tv);
+        //先select 再accept 这样就不用将socket设置成非阻塞模式了？
         printf("select ret = %d\n", ret);
         if(ret == -1) {
             perror("select");
@@ -72,60 +129,17 @@ int main() {
             continue;
         } else {
             if(FD_ISSET(serv_sock, rfds)) {
-                printf("可读！\n");
-            if( (clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size) ) < 0 ) {
-                perror("accept");
+                printf("serv_sock可读！ 也就是有新客户端connect了！\n");
+                if(acceptNaddinfo() < 0) {printf("accept failed\n");continue;}
+                printf("accept and addinfo success\n");
+            } else {
+                rebound();
+                printf("rebound successfull\n");
             }
-            s_d->client_fds[++s_d->client_cnt] = clnt_sock;
-
-            }
-
-
-
-
-
-
 
         }
-        
-
-        unsigned long ul = 1;
-        int ioctlcheck = ioctl(clnt_sock, FIONBIO, &ul);
-        printf("accept over!\n");
-        
-            //if(FD_ISSET(clnt_sock, &rfds)) printf("读 ready\n");
-            //if(FD_ISSET(clnt_sock, &wfds)) printf("写 ready\n");
-
-        if(ret < 0) {
-            perror("select error");
-        } else if(ret == 0) {
-            printf("time out!\n");
-            continue;
-        } else {
-            printf("[No. %d] select success! 准备反弹！\n", cnt);
-            char str[100];
-            //char sendstr[100] = {0};
-            int flag = 0;
-            if(FD_ISSET(clnt_sock, &rfds)) {
-                printf("clnt可读， 准备读取...\n");
-                if( recv(clnt_sock, str, sizeof(str), 0) > 0 ) {
-                    printf("读取成功 读取到的字符串是： %s , 长度： %ld\n", str, strlen(str));
-                }
-                int ret = select(clnt_sock + 1, &rfds, &wfds, NULL, &tv);
-                if(FD_ISSET(clnt_sock, &wfds)) {
-                    printf("clnt可写，准备发送...\n");
-                    //sprintf(sendstr, "[%d] %s", cnt, str);
-                    //sprintf(str, "[%d]", cnt);
-                    if( send(clnt_sock, str, strlen(str), 0) > 0 ) printf("send success\n");
-                    printf("反弹成功！\n");
-                    flag = 1;
-                }
-            }
-            if(flag == 0) printf("反弹失败!\n");
-        }
-        close(clnt_sock);
-
     }
+        
 
     close(serv_sock);
 	return 0;
